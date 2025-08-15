2b9a84952e00dbf663de502e2ef50faccf0fc25d
Dharmender Singh <webdsr77@gmail.com>
2025-08-15 19:05:24 +0530

    Chapter 7 completed

--- js.js
+++ js.js
@@ -56,9 +56,49 @@ for (box of boxs){
  } */
 
 /// Dom part 2 part Chapter 7: Date: 15th Aug 2025
+// Atributs
+/*
+let heading = document.querySelector(".main-content h2");
+console.log(heading.setAttribute("class", "subHeadBanner"));*/
+
+/*
+let headColor = document.querySelector(".main-content h2");
+headColor.style.color = "red"; 
+headColor.style.fontSize = "38px"; 
+headColor.style.fontStyle = "italic"; */
 
 
+// DOM Manipulation
+// Insert Elements
+ // - node.append( el ) // adds at the end of node (inside)
+ // - node.prepend( el ) // adds at the start of node (inside)
+ // - node.before( el ) // adds before the node (outside)
+ // - node.after( el ) //adds after the node (Outside)
 
+// Delete element
+ // node.remove() //remove the node
 
+ /* Create element */
+/*let newBtn = document.createElement("button");
+newBtn.innerText = "Join Now"; // inner text to add text inside button
+console.log(newBtn);
+
+let addButton = document.querySelector(".main-content .container");
+addButton.append(newBtn);
+//addButton.prepend(newBtn);
+//addButton.after(newBtn);
+//addButton.before(newBtn); */
+
+/*
+let removeText = document.querySelector(".main-content .container p");
+removeText.remove(); */
+
+/** add new class */
+/*
+let heading = document.querySelector(".main-content h2");
+console.log(heading.classList.add("subHeadBanner"));
+console.log(heading.classList.remove("subHeadBanner")); // This is for remove class */
+
+// Chapter 8 : Events In JS
 
 
