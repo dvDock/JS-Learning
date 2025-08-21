a9d6e60ff1d3815a88848632ad580969db712a43
Dharmender Singh <webdsr77@gmail.com>
2025-08-21 16:10:15 +0530

    New game created using js

--- js.js
+++ js.js
@@ -80,11 +80,10 @@ headColor.style.fontStyle = "italic"; */
 
  /* Create element */
 /*let newBtn = document.createElement("button");
-newBtn.innerText = "Join Now"; // inner text to add text inside button
+newBtn.innerText = "Join Us Today!"; // inner text to add text inside button
 console.log(newBtn);
-
-let addButton = document.querySelector(".main-content .container");
-addButton.append(newBtn);
+let addButton = document.querySelector(".main-content .container .subHead");
+//addButton.append(newBtn);
 //addButton.prepend(newBtn);
 //addButton.after(newBtn);
 //addButton.before(newBtn); */
@@ -97,8 +96,84 @@ removeText.remove(); */
 /*
 let heading = document.querySelector(".main-content h2");
 console.log(heading.classList.add("subHeadBanner"));
-console.log(heading.classList.remove("subHeadBanner")); // This is for remove class */
+//console.log(heading.classList.remove("subHeadBanner")); // This is for remove class */
 
 // Chapter 8 : Events In JS
+/* The change in the state of an object is known as an event
+Events are fired to notify code of "interesting changes" that may affect
+*/
+
+// Mouse events ( cancelIdleCallback, double click EventCounts. )
+/*
+let contButton = document.querySelector("#contactButton");*/
+/*
+contButton.onclick = () =>{
+    console.log("On button client event test");
+    let a = 25;
+    a++;
+    console.log(a);
+}
+
+let btn2 = document.querySelector("#contactButton2");
+btn2.onclick = () => {
+    console.log("You have hover the btn 2");
+    alert("Thank you for submiting you click");
+} */
+/*
+    contButton.addEventListener("click", () =>{
+        console.log("Button one click 1");
+    });
+contButton.addEventListener("click", () =>{
+        console.log("Button one click 2");
+    });
+    contButton.addEventListener("click", () =>{
+        console.log("Button one click 3");
+    });
+    contButton.addEventListener("click", () =>{
+        console.log("Button one click 4");
+    });
+*/
+
+// Q. Create a toggle button that change the color.
+
+/* // way one
+let modeBtn = document.querySelector("#modeBtn");
+let curMode = "light";
+
+modeBtn.addEventListener("click", () =>{
+    if(curMode === "light"){
+        curMode = "dark";
+        document.querySelector("body").style.backgroundColor = "#101010";
+        document.querySelector("body").style.color = "#fff";
+    }else {
+        curMode = "light";
+        document.querySelector("body").style.backgroundColor = "#fff";
+        ocument.querySelector("body").style.color = "#545454";
+    }
+    console.log(curMode);
+}); */
+
+/*
+ // way two - perfect example for add remove class and changes style according to class
+let modeBtn = document.querySelector("#modeBtn");
+let body = document.querySelector("body");
+let curMode = "light";
+
+modeBtn.addEventListener("click", () =>{
+    if(curMode === "light"){
+        curMode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }else {
+        curMode = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+    console.log(curMode);
+});  */
+
+/// Small game Tic Tac Toe
+
+
 
 
